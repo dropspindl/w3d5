16bf92649977136f29214240718151ee34f35092
Edelman <edelmaned@gmail.com>
2018-11-09 16:52:20 -0800

    Wrap up meta

--- w3d5_meta_edelman/lib/03_associatable.rb
+++ w3d5_meta_edelman/lib/03_associatable.rb
@@ -1,34 +1,42 @@
 require_relative '02_searchable'
 require 'active_support/inflector'
 
-# Phase IIIa
-class AssocOptions
-  attr_accessor(
-    :foreign_key,
-    :class_name,
-    :primary_key
-  )
+#
+# class AssocOptions
+#   attr_accessor(
+#     :foreign_key,
+#     :class_name,
+#     :primary_key
+#   )
+#
+#   def model_class
+#     @class_name.constantize
+#   end
+#
+#   def table_name
+#     model_class.table_name
+#   end
+# end
+#
+# class BelongsToOptions < AssocOptions
+#   def initialize(name, options = {})
+#     defaults = {
+#       :primary_key => :id,
+#       :foreign_key => "#{name}_id"
+#       :class_name => name.to_s.camelcase
+#     }
+#
+#     defaults.keys.each do |key|
+#       self.send("#{key}=", options[key] || defaults[key])
+#     end
+#   end
+# end
 
-  def model_class
-    # ...
-  end
-
-  def table_name
-    # ...
-  end
-end
-
-class BelongsToOptions < AssocOptions
-  def initialize(name, options = {})
-    # ...
-  end
-end
-
-class HasManyOptions < AssocOptions
-  def initialize(name, self_class_name, options = {})
-    # ...
-  end
-end
+# class HasManyOptions < AssocOptions
+#   def initialize(name, self_class_name, options = {})
+#     # ...
+#   end
+# end
 
 module Associatable
   # Phase IIIb
